19b3b3056c026a5c53116c2e5ad0633e8b3cf3e5
Diego Martinaglia <dm@netlive.ch>
2019-03-28 17:06:48 +0100

    working fine

--- Basic/runTaskSequentiel1.js
+++ Basic/runTaskSequentiel1.js
@@ -1,49 +1,39 @@
 'use strict';
 
 
+/* 
+  A script which executes two tasks sequencially.
+  Now we have a list of data foreach we need to execute both tasks sequencally:
+*/
 
 doTheJob();
 async function doTheJob() {
-  let promise = Promise.resolve();
   for(let i = 0;i<5;i++) {
-    let task = "task-" + i;   
-    promise = promise.then(function(result) {
-      console.log("----result=",result);
-      return doTaskSequentiel(task);
-    });    
+    let resultTask1 = await executeTask1(i);    
+    console.log("--resultTask1=",resultTask1);
+    let resultTask2 = await executeTask2(i);  
+    console.log("--resultTask2=",resultTask2);      
   }
-/*
-  for(let i = 0;i<listLinks.length;i++) {
-    let task = listLinks[i];   
-     
-
-    doTaskSequentiel(task).then(result=>{
-      console.log("---result=",null);
-    });
-    console.log("----saved---");    
-  }
-*/
-}
-
-async function doTaskSequentiel(task) {
-  let urlId = "some Value";  
-  let result = await delay(1000);
-  if (result > 100) {
-    console.log("1-------doTaskSequentiel="+task);
-    let result2 = await delay(2000);
-    console.log("2-------doTaskSequentiel="+task);
-  } else {
-    LOGGER.info("----doTaskSequentiel: entry exists----" + JSON.stringify(result[0]));
-  }
-  return urlId;
 }
 
 
-
-function delay(ms) {
+function executeTask1(data) {
+  let time2sleep   = 100;
+  let result = "result-task1-" + data; 
   return  new Promise((resolve, reject) => {
-    setTimeout(() => resolve(ms),ms);
+    setTimeout(() => resolve(result),time2sleep);
   });
 }
+
+
+
+function executeTask2(data) {
+  let time2sleep   = 200;
+  let result = "result-task2-" + data; 
+  return  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(result),time2sleep);
+  });
+}
+
 
 
